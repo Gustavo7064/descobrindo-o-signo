5e2e165dcbb709afed9d95cc98e75c90c3d1abaa
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-02-26 22:11:35 -0300

    adicionando função descobrir Signo utilizando intervalos de datas para cada signo

--- signo.js
+++ signo.js
@@ -113,3 +113,22 @@ function atualizarImagemSigno(dia, mes) {
     img.src = PASTA_IMAGENS + "vazio.png";
   }
 }
+// ==========================
+// SIGNOS
+// ==========================
+
+function descobrirSigno(dia, mes) {
+
+  if ((mes==3 && dia>=21)||(mes==4 && dia<=19)) return "Áries";
+  if ((mes==4 && dia>=20)||(mes==5 && dia<=20)) return "Touro";
+  if ((mes==5 && dia>=21)||(mes==6 && dia<=20)) return "Gêmeos";
+  if ((mes==6 && dia>=21)||(mes==7 && dia<=22)) return "Câncer";
+  if ((mes==7 && dia>=23)||(mes==8 && dia<=22)) return "Leão";
+  if ((mes==8 && dia>=23)||(mes==9 && dia<=22)) return "Virgem";
+  if ((mes==9 && dia>=23)||(mes==10 && dia<=22)) return "Libra";
+  if ((mes==10 && dia>=23)||(mes==11 && dia<=21)) return "Escorpião";
+  if ((mes==11 && dia>=22)||(mes==12 && dia<=21)) return "Sagitário";
+  if ((mes==12 && dia>=22)||(mes==1 && dia<=19)) return "Capricórnio";
+  if ((mes==1 && dia>=20)||(mes==2 && dia<=18)) return "Aquário";
+  return "Peixes";
+}
