01b30ca622e0e78e83e8084f161f362a285c8621
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-02-26 21:43:25 -0300

    criando variaveis, dando nomes e atribuindo valores

---
+++ signo.js
@@ -0,0 +1,27 @@
+
+const PASTA_IMAGENS = "fotos/";
+
+const meses = [
+  "janeiro","fevereiro","março","abril","maio","junho",
+  "julho","agosto","setembro","outubro","novembro","dezembro"
+];
+
+const diasSemana = [
+  "domingo","segunda-feira","terça-feira","quarta-feira",
+  "quinta-feira","sexta-feira","sábado"
+];
+
+const arquivoPorSigno = {
+  "Áries": "aries.jpg",
+  "Touro": "touro.jpg",
+  "Gêmeos": "gemeos.jpg",
+  "Câncer": "Cancer.jpg",
+  "Leão": "leao.jpg",
+  "Virgem": "virgem.jpg",
+  "Libra": "libra1.webp",
+  "Escorpião": "escorpiao.jpg",
+  "Sagitário": "sagitario.jpg",
+  "Capricórnio": "capricornio.jpg",
+  "Aquário": "Aquario.jpg",
+  "Peixes": "peixes.jpg"
+};
